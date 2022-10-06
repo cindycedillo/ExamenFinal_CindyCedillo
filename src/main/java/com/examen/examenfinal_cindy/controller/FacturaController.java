36871d7ea88f7d2038d7c98927361aed54ab1d24
Cindy Cedillo <cindy.cedillo.est@tecazuay.edu.ec>
2022-10-06 10:20:29 -0500

    Factura controller

--- src/main/java/com/examen/examenfinal_cindy/controller/FacturaController.java
+++ src/main/java/com/examen/examenfinal_cindy/controller/FacturaController.java
@@ -5,10 +5,54 @@
  */
 package com.examen.examenfinal_cindy.controller;
 
+import com.examen.examenfinal_cindy.model.Factura;
+import com.examen.examenfinal_cindy.model.Producto;
+import com.examen.examenfinal_cindy.service.FacturaService;
+import com.examen.examenfinal_cindy.service.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author TUF Gaming
  */
+
+@RestController
+@RequestMapping("/api/factura")
 public class FacturaController {
+      @Autowired
+    FacturaService productoService;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Factura>> listar() {
+        return new ResponseEntity<>(productoService.findByAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/listarCodigo/{id}")
+    public Factura encontrarCodigo(@PathVariable Long id) {
+        return productoService.findById(id);
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<Factura> crearCliente(@RequestBody Factura factura) {
+        return new ResponseEntity<>(productoService.save(factura), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Factura> eliminarCliente(@PathVariable Long id) {
+        productoService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+    
     
 }
