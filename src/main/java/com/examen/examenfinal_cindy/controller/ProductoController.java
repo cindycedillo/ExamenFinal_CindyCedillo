64f92dd45a4ce4e0dead49d044e51be0e231a411
Cindy Cedillo <cindy.cedillo.est@tecazuay.edu.ec>
2022-10-06 10:17:06 -0500

    Producto Controller

--- src/main/java/com/examen/examenfinal_cindy/controller/ProductoController.java
+++ src/main/java/com/examen/examenfinal_cindy/controller/ProductoController.java
@@ -5,10 +5,49 @@
  */
 package com.examen.examenfinal_cindy.controller;
 
+import com.examen.examenfinal_cindy.model.Cliente;
+import com.examen.examenfinal_cindy.model.Producto;
+import com.examen.examenfinal_cindy.service.ClienteService;
+import com.examen.examenfinal_cindy.service.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 /**
  *
  * @author TUF Gaming
  */
 public class ProductoController {
+    
+     @Autowired
+    ProductoService productoService;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Producto>> listar() {
+        return new ResponseEntity<>(productoService.findByAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/listarCodigo/{id}")
+    public Producto encontrarCodigo(@PathVariable Long id) {
+        return productoService.findById(id);
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<Producto> crearCliente(@RequestBody Producto persona) {
+        return new ResponseEntity<>(productoService.save(persona), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Producto> eliminarCliente(@PathVariable Long id) {
+        productoService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
     
 }
